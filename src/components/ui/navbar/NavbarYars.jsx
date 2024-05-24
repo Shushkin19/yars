4057eabcd162c8c286528cfcceae3c5e128c666b
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2024-05-25 00:28:22 +0300

    fixed display of cards

--- src/components/ui/navbar/NavbarYars.jsx
+++ src/components/ui/navbar/NavbarYars.jsx
@@ -5,6 +5,7 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import "../navbar/NavbarYars.module.css"
 
 import ModalYars from "../modal/ModalYars";
 
